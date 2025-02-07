eeba67e3ad0fca20483183dc3eaa650f142f0429
Bhuwan Chhimal <118724329+BhuwanChhimal@users.noreply.github.com>
2025-02-07 16:18:34 +0545

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2023 Bh-C Headphones. All rights reserved</p>
+      <p> &copy; {new Date().getFullYear()} Bh-C Headphones. All rights reserved</p>
       <p className="icons">
         <AiFillInstagram/>
         <AiOutlineTwitter/>
