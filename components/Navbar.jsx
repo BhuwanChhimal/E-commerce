32eb7531a0b71b57b40c03a51bd756544a0e8cbf
Bhuwan Chhimal <118724329+BhuwanChhimal@users.noreply.github.com>
2025-02-27 20:05:17 +0545

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href='/'>BAAI-VERSE</Link>
+        <Link href='/'>Wearables-X</Link>
       </p>
 
       <button type='button' 
