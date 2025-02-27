d8c1264166858b3f4f59ce8227aacde368826cdb
Bhuwan Chhimal <118724329+BhuwanChhimal@users.noreply.github.com>
2025-02-27 20:04:59 +0545

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
       <Head>
-        <title>Bh-C Store</title>
+        <title>Wearables-X</title>
       </Head>
       <header>
         <Navbar/>
